1ac37ad574ed82676169c816db190a4a65b31e5d
b3ng <rubenhidayat02@gmail.com>
2025-11-30 20:52:42 +0700

    Prepare for version 1.0 release
    - added computer choice
    - added human choice
    - added play logic
    - added final score logic

--- styles.js
+++ styles.js
@@ -1,8 +1,80 @@
 // getComputerChoice
+function getComputerChoice(){
+    let random = Math.floor(Math.random() * 3);
+    if (random === 0){
+        return "rock";
+    }else if (random === 1){
+        return "paper";
+    }else{
+        return "scissors"
+    }
+} 
 
 // getHumanChoice
+function getHumanChoice(){
+    while (true){
+        let choices = prompt(`round ${rounds}\nhuman: ${humanScore}\ncomputer: ${computerScore}`).toLowerCase();
+        let choice = choices.toLowerCase();
+        if (choice === "rock" || choice === "paper" || choice === "scissors") {
+            return choice;
+        }
+    }
+}
 
-// While Loop 5 Rounds
+// playRound (round logic)
+function playRound(humanSelection, computerSelection){
+    const match = `${humanSelection} - ${computerSelection}`
 
-// Playtround
+    switch (match){
+        // --- DRAW CASES ---
+        case "paper - paper":
+        case "scissors - scissors":
+        case "rock - rock":
+            console.log("Draw!\n"+match);
+            break;
 
+        // -- HUMAN WINS CASES --
+        case "rock - scissors":
+        case "paper - rock":
+        case "scissors - paper":
+            console.log("Human Wins!\n"+match);
+            humanScore += 1;
+            break;
+        
+        // -- COMPUTER WINS CASES -- 
+        case "rock - paper":
+        case "paper - scissors":
+        case "scissors - rock":
+            console.log("Computer Wins!\n"+match);
+            computerScore +=1;
+            break
+    }
+}
+
+// while Loop 5 Rounds
+rounds = 1;
+let humanScore = 0;
+let computerScore = 0;
+while (true){
+    if (rounds === 6){
+        if (humanScore > computerScore){
+            console.log("Game Over, Human Wins")
+        }else if (computerScore < humanScore){
+            console.log("Game Over, Computer Wins!")
+        }else{
+            console.log("DRAW!!!")
+        }
+        break
+    }
+    console.log(`round: ${rounds}`)
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+
+    
+    
+
+    rounds++;
+}
+    
